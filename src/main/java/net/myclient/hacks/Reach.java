0cdf639cb2c396e687b061f9948c5ed7baa33642
GamesDoneWr0ng <ask@flaten.no>
2023-01-31 20:12:33 +0100

    Reach get target

--- src/main/java/net/myclient/hacks/Reach.java
+++ src/main/java/net/myclient/hacks/Reach.java
@@ -1,22 +1,21 @@
 package net.myclient.hacks;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.Packet;
 import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
-import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.myclient.events.SentPacketListener;
 import net.myclient.hack.Hack;
-import net.myclient.util.TargetedEntityFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Reach extends Hack implements SentPacketListener {
     private Packet<?> lastpacket;
@@ -25,46 +24,45 @@ public class Reach extends Hack implements SentPacketListener {
     @Override
     public void onSentPacket(SentPacketEvent event) {
         Packet<?> packet = event.getPacket();
-        if (lastpacket instanceof PlayerInteractEntityC2SPacket || lastpacket instanceof PlayerActionC2SPacket) {return;}
+        if (lastpacket instanceof PlayerInteractEntityC2SPacket || lastpacket instanceof PlayerActionC2SPacket) {
+            lastpacket = null;
+            return;
+        }
         lastpacket = packet;
 
         if (packet instanceof HandSwingC2SPacket) {
             PlayerEntity player = MinecraftClient.getInstance().player;
+            ClientWorld world = MinecraftClient.getInstance().world;
             assert player != null;
-            Vec3d viewDir = getViewDirection(player);
-            List<Entity> entities = (List<Entity>) MinecraftClient.getInstance().world.getEntities();
-            TargetedEntityFilter filter = new TargetedEntityFilter(aimAssist, player.getPos(), viewDir);
-            List<Entity> filteredEntities = entities.stream().filter(filter::test).toList();
-            Entity closest = getClosestEntity(filteredEntities, player.getPos(), viewDir);
+            Entity closest = getEntityFromRay(world, player, 50);
 
             LOGGER.info("Entity: {}", closest);
         }
     }
 
-    public Vec3d getViewDirection(PlayerEntity player) {
-        float pitch = player.getPitch();
-        float yaw = player.getYaw();
-        float x = MathHelper.cos(-yaw * 0.017453292F - (float) Math.PI);
-        float z = MathHelper.sin(-yaw * 0.017453292F - (float) Math.PI);
-        float y = -MathHelper.cos(-pitch * 0.017453292F);
-        return new Vec3d(x, y, z);
-    }
+    public static Entity getEntityFromRay(World world, PlayerEntity player, double range) {
+        Vec3d playerPos = player.getEyePos();
+        Vec3d lookVec = player.getRotationVec(1.0F);
+        Vec3d endPos = playerPos.add(lookVec.multiply(range));
 
-    public Entity getClosestEntity(List<Entity> entities, Vec3d playerPos, Vec3d viewDir) {
-        Entity closest = null;
-        double minDistance = Double.MAX_VALUE;
+        List<Entity> entities = world.getEntitiesByClass(Entity.class, player.getBoundingBox().expand(lookVec.x * range, lookVec.y * range, lookVec.z * range).expand(1.0D), entity -> !entity.isSpectator());
+
+        Entity closestEntity = null;
+        double closestDistance = Double.MAX_VALUE;
+
         for (Entity entity : entities) {
             Vec3d entityPos = entity.getPos();
-            Vec3d rayToEntity = entityPos.subtract(playerPos);
-            double distanceAlongRay = rayToEntity.dotProduct(viewDir);
-            Vec3d closestPoint = playerPos.add(viewDir.multiply(distanceAlongRay));
-            double distanceToClosestPoint = entityPos.distanceTo(closestPoint);
-            if (distanceToClosestPoint < minDistance) {
-                closest = entity;
-                minDistance = distanceToClosestPoint;
+            Vec3d rayVec = entityPos.subtract(playerPos);
+
+            if (lookVec.normalize().dotProduct(rayVec.normalize()) > Math.cos(Math.toRadians(5))) {
+                double distance = rayVec.lengthSquared();
+                if (distance < closestDistance) {
+                    closestEntity = entity;
+                    closestDistance = distance;
+                }
             }
         }
-        return closest;
+        return closestEntity;
     }
 
     @Override
